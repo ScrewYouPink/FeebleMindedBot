70b5b32a83b2b81d631f19b05afd3b66b03a1775
DonJTru <43910974+DonJTru@users.noreply.github.com>
2020-10-12 15:12:18 -0700

    1.0.4

--- commands/log2.0.js
+++ commands/log2.0.js
@@ -1,22 +1,39 @@
 const { MessageEmbed} = require("discord.js");
-const config = require("../config/client.json")
+const { LOGO } = require(`../config/MyVarbs.json`);
+const fs = require("fs")
+
 
 module.exports = {
     name: 'log',
     description: 'test for new log system!',
     type: 'UTIL',
     execute(message){
-        SID = message.guild.id
-        var thisServer = config.guilds[SID]
-        var log = thisServer.log
-        if (log) {
-            var embed = new MessageEmbed()
-            .setAuthor(`**LOG**`, )
-            .setThumbnail(message.author.displayAvatarURL())
-            .setColor('#FFAE00')
-            .setDescription(`**TEST** A Test of Log 2.0 happened`)
-                embed.setTimestamp();
-                logChannel.send(embed)
+
+        var data = '';
+        var readStream = fs.createReadStream('./config/client.json', 'utf8');
+        readStream.on('data', function(chunk) {
+            data += chunk;
+        }).on('end', function() {
+            var config = JSON.parse(data)
+            SID = message.guild.id
+            var thisServer = config.guilds[SID]
+            var log = thisServer.log
+            if (log) {
+
+
+                var embed = new MessageEmbed()
+                .setAuthor(`LOG`, LOGO)
+                .setThumbnail(LOGO)
+                .setColor('#FFAE00')
+                .setDescription(`**TEST** A Test of Log 2.0 happened`)
+                    embed.setTimestamp();
+
+
+
+                    message.client.channels.cache.get(thisServer.logchannel).send(embed)
+                }
+            })
+
+
         }
     }
-}
